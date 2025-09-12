f1b4fcf8aaf8e0b51aceb9d7e5930172a7c4bac8
NellitG <kamaunelson157@gmail.com>
2025-09-12 10:26:50 +0300

    Add Booking restriction to admin only view bookings made by users

--- booking/admin.py
+++ booking/admin.py
@@ -2,6 +2,14 @@ from django.contrib import admin
 from .models import Car, Booking, Driver
 
 # Register your models here.
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display =("department", "destination", "status")
+
+    def has_add_permission(self, request):
+        if request.user.is_superuser:
+            return False
+        return super().has_add_permission(request)
+    
 admin.site.register(Car)
-admin.site.register(Booking)
 admin.site.register(Driver)
