f24cfebea17c91f582aee2d2bc366c91efff2d01
NellitG <kamaunelson157@gmail.com>
2025-09-12 10:28:55 +0300

    Add permission to Manager, Admin and Employee

--- booking/permissions.py
+++ booking/permissions.py
@@ -2,17 +2,16 @@ from rest_framework.permissions import BasePermission
 
 class IsManagerOrAdmin(BasePermission):
     def has_permission(self, request, view):
-        user = request.user
-        return bool(user and (user.role == 'MANAGER' or user.role == 'ADMIN'))
+        return request.user.is_authenticated and request.user.role in ['manager', 'admin']
     
 class IsEmployee(BasePermission):
     def has_permission(self, request, view):
-        user = request.user
-        return bool(user and user.role == 'EMPLOYEE')
-    
-class IsOwnerOrManager(BasePermission):
-    def has_object_permission(self, request, view, obj):
-        user = request.user
-        if user.role == ['MANAGER', 'ADMIN']:
-            return True
-        return obj.requested_by == user
+        return request.user.is_authenticated and request.user.role=='employee'
+
+class CanApproveBooking(BasePermission):
+    def has_permission(self, request, view):
+        if request.method == 'POST':
+            return request.user.groups.filter(name='employee').exists()
+        if request.method in ['PUT', 'PATCH', 'DELETE']:
+            return request.user.groups.filter(name__in=['manager', 'admin']).exists()
+        return True
