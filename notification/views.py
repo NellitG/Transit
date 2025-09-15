a4427036e42f6646d4acc8959646953606d827a4
NellitG <kamaunelson157@gmail.com>
2025-09-15 15:50:46 +0300

    Added explanation of class methods and improved readability

--- notification/views.py
+++ notification/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from rest_framework.permissions import IsAuthenticated
+from .serializers import NotificationSerializer
+from .models import Notification
+from rest_framework import permissions
 
 # Create your views here.
+class NotificationViewSet(viewsets.ReadOnlyModelViewSet):
+    serializer_class = NotificationSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_queryset(self):
+        return Notification.objects.filter(user=self.request.user).order_by('-created_at')
