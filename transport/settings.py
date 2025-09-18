1dcd01aadf3dce85af120ab1cd45c16178aef254
NellitG <kamaunelson157@gmail.com>
2025-09-18 15:19:35 +0300

    Add cors header to allow frontend/backend communication

--- transport/settings.py
+++ transport/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'accounts',
     'booking',
     'notification',
+    'corsheaders',
 ]
 
 REST_FRAMEWORK = {
@@ -59,7 +60,10 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'transport.urls'
 
